3cfa134de92742f477297fbb6f31def2e1de3005
Rahul <rahul.shilwant31@gmail.com>
2020-09-09 19:31:06 +0530

    edit changes

---
+++ readinput.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/bash
+
+# echo "Enter name: "
+# read name
+
+# echo "Enter name is:" $name
+
+
+
+
+
+# echo "enter name :"
+# read name1 name2 name3
+
+# echo "Names :" $name1  $name2  $name3
+
+
+
+# read -p "username :"  var
+# echo "username is : "$var
+
+
+echo "enter name :"
+read -a
+echo "name : ${name[0]} ,${name[1]}"
---
+++ variable.sh
@@ -0,0 +1,14 @@
+#! /usr/bin/bash
+
+echo "hello  world"
+
+
+
+# variables
+
+name=Rahul
+val=10
+
+echo the name is $name
+echo the name is $val
+
